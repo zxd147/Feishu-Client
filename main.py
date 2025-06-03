72483f56abb4f559ab6982c58532b3e2c30c6055
zxd147 <z238182822@gmail.com>
2025-06-03 10:33:38 +0800

    change log code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from controllers.feishu_robot import FeishuRobot
+from utils.logger import setup_logger, get_loger
+
 
 if __name__ == "__main__":
+    setup_logger()
+    logger = get_loger()
+    logger.info("Service starting...")
     feishu_robot = FeishuRobot()
     feishu_robot.start()
 
