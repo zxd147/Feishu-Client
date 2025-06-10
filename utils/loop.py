a7e143841bb9598d8e49ff6252e6d864a84db067
zxd147 <z238182822@gmail.com>
2025-06-11 00:50:35 +0800

    update!

---
+++ utils/loop.py
@@ -0,0 +1,9 @@
+import asyncio
+
+def get_loop():
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    return loop
